1de52413e168cb1ee1f41548f015ea0245d22493
Jacob Parker <jacobparker1992@gmail.com>
2016-05-14 14:12:00 +0100

    Initial Commit

---
+++ playback-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  playback-project
+//
+//  Created by Jacob Parker on 14/05/2016.
+//  Copyright © 2016 Jacob Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
