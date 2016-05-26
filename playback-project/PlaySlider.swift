bd7f403ff4eaafa4b0c1559a57480c898fa110e6
Jacob Parker <jacobparker1992@gmail.com>
2016-05-26 18:23:57 +0100

    Cleanup

--- playback-project/PlaySlider.swift
+++ playback-project/PlaySlider.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class PlaySlider: UIControl {
 
     @IBInspectable
@@ -22,6 +23,8 @@ class PlaySlider: UIControl {
     var maximum: Double = 100 { didSet { setNeedsLayout() } }
 
     private var playPauseButton = ShapeButton()
+    private var previousButton = ShapeButton()
+    private var nextButton = ShapeButton()
     private var sliderSize: CGFloat {
         get {
             return frame.size.height
@@ -29,26 +32,56 @@ class PlaySlider: UIControl {
     }
     private var sliderPosition: CGFloat {
         get {
-            return (frame.size.width - frame.size.height) * CGFloat(value / (maximum - minimum))
+            return (frame.size.width - 3 * sliderSize) * CGFloat(value / (maximum - minimum))
         }
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setup()
+    }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    func setup() {
         addSubview(playPauseButton)
+        addSubview(previousButton)
+        addSubview(nextButton)
     }
 
     override func layoutSubviews() {
-        playPauseButton.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: sliderSize)
+        let width = frame.size.width
+        let sliderWidth = width - 2 * sliderSize
 
-        let path = UIBezierPath(ovalInRect: CGRect(x: sliderPosition, y: 0, width: sliderSize, height: sliderSize))
-        path.moveToPoint(CGPoint(x: bounds.minX, y: bounds.midY))
-        path.addLineToPoint(CGPoint(x: sliderPosition, y: playPauseButton.frame.midY))
-        path.moveToPoint(CGPoint(x: sliderPosition + sliderSize, y: playPauseButton.frame.midY))
-        path.addLineToPoint(CGPoint(x: bounds.maxX, y: bounds.midY))
+        previousButton.frame = CGRect(x: 0, y: 0, width: sliderSize, height: sliderSize)
+        playPauseButton.frame = CGRect(x: sliderSize, y: 0, width: sliderWidth, height: sliderSize)
+        nextButton.frame = CGRect(x: width - sliderSize, y: 0, width: sliderSize, height: sliderSize)
 
-        playPauseButton.path = path.CGPath
+        let playPausePath = UIBezierPath(ovalInRect: CGRect(
+            x: sliderPosition + lineWidth / 2,
+            y: lineWidth / 2,
+            width: sliderSize - lineWidth,
+            height: sliderSize - lineWidth
+        ))
+        playPausePath.moveToPoint(CGPoint(x: 0, y: bounds.midY))
+        playPausePath.addLineToPoint(CGPoint(x: sliderPosition, y: playPauseButton.frame.midY))
+        playPausePath.moveToPoint(CGPoint(x: sliderPosition + sliderSize, y: playPauseButton.frame.midY))
+        playPausePath.addLineToPoint(CGPoint(x: sliderWidth, y: bounds.midY))
+
+        playPauseButton.path = playPausePath.CGPath
+
+        let previousNextPath = UIBezierPath(ovalInRect: CGRect(
+            x: lineWidth / 2,
+            y: lineWidth / 2,
+            width: sliderSize - lineWidth,
+            height: sliderSize - lineWidth
+        ))
+
+        previousButton.path = previousNextPath.CGPath
+        nextButton.path = previousNextPath.CGPath
     }
 
 }
--- playback-project/SliderView.swift
+++ playback-project/SliderView.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Foundation
 
+@IBDesignable
 class SliderView: UIControl {
 
     @IBInspectable
